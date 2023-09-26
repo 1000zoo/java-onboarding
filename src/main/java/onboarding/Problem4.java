3633f06261cb6b308f30a9a9c5aab3d6751f9e31
1000zoo <cjswldn99@gmail.com>
2023-09-26 19:01:53 +0900

    feat(onboarding): 4번 문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertString(word);
+    }
+
+    private static String convertString(String word) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : word.toCharArray()) {
+            sb.append(convertCharacter(c));
+        }
+        return sb.toString();
+    }
+
+    private static char convertCharacter(char c) {
+        if (c == ' ') {
+            return c;
+        }
+        return convertCase(c);
+    }
+
+    private static char convertCase(char c) {
+        char converted = convertAlphabet(Character.toLowerCase(c));
+        if (Character.isUpperCase(c)) {
+            return Character.toUpperCase(converted);
+        }
+        return converted;
+    }
+
+    private static char convertAlphabet(char c) {
+        return (char) ('z' - c + 'a');
     }
 }
