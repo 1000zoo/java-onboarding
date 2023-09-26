e72039b8d6aefa9f6eb76e7122ba5245268c6079
1000zoo <cjswldn99@gmail.com>
2023-09-26 17:05:56 +0900

    feat(onboarding): 2번문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decoder(cryptogram);
+    }
+
+    private static String decoder(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : cryptogram.toCharArray()) {
+            if (stack.empty() || stack.peek() != c) {
+                stack.push(c);
+                continue;
+            }
+            stack.pop();
+        }
+
+        return stackToString(stack);
+    }
+
+    private static String stackToString(Stack<Character> stack) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : stack) {
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
 }
